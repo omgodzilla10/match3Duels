c19f98c6bbe8e35660debdcf74852add0da958b0
omgodzilla10 <trevorhoef@gmail.com>
2015-12-14 18:59:56 -0800

    Cleaned up deprecated code.

--- projectFolder/core/src/com/mygdx/game/Match3Duels_Game.java
+++ projectFolder/core/src/com/mygdx/game/Match3Duels_Game.java
@@ -24,32 +24,26 @@ public class Match3Duels_Game implements Screen {
     /** The maximum number of gems allowed to be in use. */
     final int MAX_GEMS = 5;
     
+    /** The width of the screen (at execution). */
     private int screenWidth;
+    
+    /** The height of the screen (at execution). */
     private int screenHeight;
     
-    /*
-    private Texture gemIcon_fire_01;
-    private Texture gemIcon_lightning_01;
-    private Texture gemIcon_poison_01;
-    private Texture gemIcon_shield_01;
-    private Texture gemIcon_heal_01;
-    
-    private Texture[] iconArray;
-    private Texture[][] boardIconArray;
-    
-    private Actor gem_fire_01;
-    private Actor gem_lightning_01;
-    private Actor gem_poison_01;
-    private Actor gem_shield_01;
-    private Actor gem_heal_01;*/
-    
-    private Actor[] gemArray;
+    /** An array for each of the gem actors on the board. */
     private Actor[][] boardGemArray;
     
+    /** The stage on which the board and UI elements will appear. */
     private Stage gameStage;
     
+    /** The stage's viewport. */
     private StretchViewport sViewport;
     
+    /** The constructor. 
+     * 
+     * @param game - Passed in when the game is launched 
+     *               from the main menu.
+     */
     public Match3Duels_Game(final Match3Duels_Main game) {
         this.game = game;
         
@@ -58,37 +52,6 @@ public class Match3Duels_Game implements Screen {
         
         sViewport = new StretchViewport(screenWidth, screenHeight);
         gameStage = new Stage(sViewport);
-        
-        /*
-        //Create each gemIcon texture object.
-        gemIcon_fire_01 = new Texture(Gdx.files.internal("gem_fire_01.png"));
-        gemIcon_lightning_01 = new Texture(Gdx.files.internal("gem_lightning_01.png"));
-        gemIcon_poison_01 = new Texture(Gdx.files.internal("gem_poison_01.png"));
-        gemIcon_shield_01 = new Texture(Gdx.files.internal("gem_shield_01.png"));
-        gemIcon_heal_01 = new Texture(Gdx.files.internal("gem_heal_01.png"));
-        
-        //Assign each gem icon to the gemIcon array.
-        iconArray = new Texture[MAX_GEMS];
-        iconArray[0] = gemIcon_fire_01;
-        iconArray[1] = gemIcon_lightning_01;
-        iconArray[2] = gemIcon_poison_01;
-        iconArray[3] = gemIcon_shield_01;
-        iconArray[4] = gemIcon_heal_01;
-        
-        //Create each gem actor.
-        gem_fire_01 = new FireGemActor_01();
-        gem_lightning_01 = new Actor();
-        gem_poison_01 = new Actor();
-        gem_shield_01 = new Actor();
-        gem_heal_01 = new Actor();
-        
-        //Assign each gem to the gemArray
-        gemArray = new Actor[MAX_GEMS];
-        gemArray[0] = gem_fire_01;
-        gemArray[1] = gem_lightning_01;
-        gemArray[2] = gem_poison_01;
-        gemArray[3] = gem_shield_01;
-        gemArray[4] = gem_heal_01;*/
         
         //Initialize the board arrays with random gems
         boardGemArray = new Actor[BOARD_ROWS][BOARD_ROWS];
@@ -114,9 +77,6 @@ public class Match3Duels_Game implements Screen {
                         + (screenWidth / BOARD_ROWS / 4), (col * (screenWidth / BOARD_ROWS)) 
                         + (screenWidth / BOARD_ROWS / 4));
                 
-                /*boardGemArray[row][col].setPosition((row * (screenWidth / BOARD_ROWS)), 
-                        (col * (screenWidth / BOARD_ROWS)));*/
-                
                 gameStage.addActor(boardGemArray[row][col]);
             }
         }
@@ -126,20 +86,6 @@ public class Match3Duels_Game implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        
-        /*
-        game.batch.begin();
-        
-        //Render each gem in the board
-        for(int row = 0; row < BOARD_ROWS; row++) {
-            for(int col = 0; col < BOARD_ROWS; col++) {
-                game.batch.draw(boardIconArray[row][col], boardGemArray[row][col].x, 
-                        boardGemArray[row][col].y, boardGemArray[row][col].width, 
-                        boardGemArray[row][col].height);
-            }
-        }
-        
-        game.batch.end();*/
         
         gameStage.act(delta);
         gameStage.draw();
